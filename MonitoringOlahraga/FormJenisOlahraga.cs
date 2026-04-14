12f21febe048be8450f83a8d3afc946daa452814
ArbyPangestu <arbypangestu340@gmail.com>
2026-04-15 06:37:57 +0700

    Menambahkan fitur hapus data jenis olahraga berdasarkan ID dengan pop-up konfirmasi (MessageBox)

--- MonitoringOlahraga/FormJenisOlahraga.cs
+++ MonitoringOlahraga/FormJenisOlahraga.cs
@@ -44,6 +44,44 @@ namespace MonitoringOlahraga
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                DialogResult resultConfirm = MessageBox.Show(
+                    "Yakin ingin menghapus data?",
+                    "Konfirmasi",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (resultConfirm == DialogResult.Yes)
+                {
+                    string query = "DELETE FROM JenisOlahraga WHERE id_jenis = @id_jenis";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id_jenis", txtIdJenis.Text);
+
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("Data berhasil dihapus");
+                        ClearForm();
+                        btnLoad.PerformClick();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data tidak ditemukan");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+            }
         }
 
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
